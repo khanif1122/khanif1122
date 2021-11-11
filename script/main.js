56003bb3fcc225ef57b296b323a261cb6cc496f8
khanif1122 <93782577+khanif1122@users.noreply.github.com>
2021-11-12 02:33:31 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -116,28 +116,28 @@ const animationTimeline = () => {
       },
       "+=0.7"
     )
-    .from(".idea-1", 0.1, ideaTextTrans)
-    .to(".idea-1", 0.1, ideaTextTransLeave, "+=0.1")
-    .from(".idea-2", 0.1, ideaTextTrans)
-    .to(".idea-2", 0.1, ideaTextTransLeave, "+=0.1")
-    .from(".idea-3", 0.1, ideaTextTrans)
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
-      scale: 0.5,
-      x: 5,
+      scale: 1.5,
+      x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
-    .to(".idea-3", 0.1, ideaTextTransLeave, "+=0.5")
-    .from(".idea-4", 0.1, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=0.5")
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
       ".idea-5",
       0.1, {
-        rotationX: 5,
-        rotationZ: -5,
+        rotationX: 25,
+        rotationZ: -20,
         skewY: "-5deg",
-        y: 25,
-        z: 5,
+        y: 70,
+        z: 20,
         opacity: 0,
       },
       "+=0.5"
